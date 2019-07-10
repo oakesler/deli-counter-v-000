aa99e14d39bdbf5f8a2a21479096a4fdecd3d928
Olivia Kesler <oakesler@protonmail.ch>
2019-07-10 17:56:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,11 +10,16 @@ def line(katz_deli)
 def take_a_number(katz_deli, name)
   line_array = [ ]
   num = 0
-  while num < katz_deli.length
-    line_array.push("#{x + 1}. #{katz_deli[x]}")
-    x += 1 
-  end
+  if katz_deli.length == 0
+    puts "The line is empty."
+  
+  elsif katz_deli.length > 0 
+    while num < katz_deli.length
+      line_array.push("#{x + 1}. #{katz_deli[x]}")
+      x += 1 
+    end
   puts "The line is currently: #{line_array} "
+end
 end
   
 def now_serving
